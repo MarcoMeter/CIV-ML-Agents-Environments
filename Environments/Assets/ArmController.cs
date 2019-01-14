09a8635bedc0172841968984e62808d504108c99
Fabian Fritzsche <mail@fabian-fritzsche.de>
2019-01-14 14:18:01 +0100

    arm controller arm motor hook logic (hook Rigidbodies to hook using trigger spheres)

---
+++ Environments/Assets/ArmController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class ArmController : MonoBehaviour
+{
+    public ArmMotor motor;
+    public Animator hookAnimator;
+    public HookLogic hookLogic;
+
+    private void Update()
+    {
+        var leftInput = default(Vector3);
+        if (Input.GetKey(KeyCode.W))
+            leftInput.z += 1;
+
+        if (Input.GetKey(KeyCode.S))
+            leftInput.z -= 1;
+
+        if (Input.GetKey(KeyCode.A))
+            leftInput.x -= 1;
+
+        if (Input.GetKey(KeyCode.D))
+            leftInput.x += 1;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            leftInput.y += 1;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            leftInput.y -= 1;
+        motor.MoveInput = leftInput;
+
+
+        var rightInput = 0f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rightInput -= 1;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            rightInput += 1;
+        motor.RotateInput = rightInput;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            bool state = hookAnimator.GetBool("Closed");
+            hookAnimator.SetBool("Closed", !state);
+            hookLogic.Attach(state);
+        }
+    }
+}
---
+++ Environments/Assets/HookLogic.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HookLogic : MonoBehaviour
+{
+    public LayerMask lidLayer;
+    public Collider attachCollider;
+    public FixedJoint fixedJoint;
+
+    private GameObject storedLid;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (1 << other.gameObject.layer != lidLayer)
+            return;
+
+        storedLid = other.gameObject;
+
+        Debug.Log($"Found {other.name}");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (storedLid == null)
+            return;
+
+        if (other.gameObject != storedLid)
+            return;
+
+        storedLid = null;
+
+        Debug.Log($"Lost {other.name}");
+    }
+
+    public void Attach(bool state)
+    {
+        if (storedLid == null)
+            return;
+
+        if (state == false)
+        {
+            storedLid.transform.position = attachCollider.transform.position;
+            storedLid.transform.rotation = attachCollider.transform.rotation;
+
+            fixedJoint = gameObject.AddComponent<FixedJoint>();
+            fixedJoint.connectedBody = storedLid.transform.GetComponentInParent<Rigidbody>();
+            fixedJoint.connectedBody.useGravity = false;
+        }
+        else
+        {
+            fixedJoint.connectedBody.useGravity = true;
+            StartCoroutine(WaitOneFixedTimeStepWakeup(fixedJoint.connectedBody));
+            Destroy(fixedJoint);
+        }
+    }
+
+    private IEnumerator WaitOneFixedTimeStepWakeup(Rigidbody rigidbody)
+    {
+        yield return new WaitForFixedUpdate();
+
+        rigidbody.WakeUp();
+    }
+}
